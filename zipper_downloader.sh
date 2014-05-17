5903a6702c782445aefd4a97767823ad58e4741e
Mitchel Humpherys <mitch.special@gmail.com>
2014-05-17 10:57:19 -0700

    zipper_downloader.sh: verbose flag

--- zipper_downloader.sh
+++ zipper_downloader.sh
@@ -2,24 +2,26 @@
 
 cd $(dirname $0)
 
+VERBOSE=${VERBOSE:-0}
+
 d=$(date +"%Y-%m-%dT%H:%M:%S%z")
 zipurl="http://zipperdog.dyndns.org/NWS"
 
 wget_opts="--quiet -O-"
 
-echo -n "Downloading images [$d]..."
+[[ $VERBOSE -eq 0 ]] || echo -n "Downloading images [$d]..."
 c1out=camera1/$(date +%Y)/$(date +%m)/$(date +%d)
 mkdir -p $c1out
 wget ${=wget_opts} $zipurl/camera1.jpg > $c1out/camera1_${d}.jpg
 
-echo -n .
+[[ $VERBOSE -eq 0 ]] || echo -n .
 c2out=camera2/$(date +%Y)/$(date +%m)/$(date +%d)
 mkdir -p $c2out
 wget ${=wget_opts} $zipurl/camera2.jpg > $c2out/camera2_${d}.jpg
 
-echo -n .
+[[ $VERBOSE -eq 0 ]] || echo -n .
 c3out=camera3/$(date +%Y)/$(date +%m)/$(date +%d)
 mkdir -p $c3out
 wget ${=wget_opts} $zipurl/camera3.jpg > $c3out/camera3_${d}.jpg
 
-echo done
+[[ $VERBOSE -eq 0 ]] || echo done
