83ac141e90301b2b751c0d447e35aefd6952bde9
Mitchel Humpherys <mitch.special@gmail.com>
2014-06-20 16:47:33 -0700

    Revert "Revert "zipper_downloader.sh: new download URLs""
    This reverts commit 81350b17da7fde7faaf43ecb8bd46dff9abaec30.

--- zipper_downloader.sh
+++ zipper_downloader.sh
@@ -5,23 +5,23 @@ cd $(dirname $0)
 VERBOSE=${VERBOSE:-0}
 
 d=$(date +"%Y-%m-%dT%H:%M:%S%z")
-zipurl="http://zipperdog.dyndns.org/NWS"
+zipurl="http://zipperdog.dyndns.org/Images"
 
 wget_opts="--quiet -t 2 --connect-timeout=10 -O-"
 
 [[ $VERBOSE -eq 0 ]] || echo -n "Downloading images [$d]..."
 c1out=camera1/$(date +%Y)/$(date +%m)/$(date +%d)
 mkdir -p $c1out
-wget ${=wget_opts} $zipurl/camera1.jpg > $c1out/camera1_${d}.jpg
+wget ${=wget_opts} $zipurl/hyrum_dam_w.jpg > $c1out/camera1_${d}.jpg
 
 [[ $VERBOSE -eq 0 ]] || echo -n .
 c2out=camera2/$(date +%Y)/$(date +%m)/$(date +%d)
 mkdir -p $c2out
-wget ${=wget_opts} $zipurl/camera2.jpg > $c2out/camera2_${d}.jpg
+wget ${=wget_opts} $zipurl/hyrum_dam_ne.jpg > $c2out/camera2_${d}.jpg
 
 [[ $VERBOSE -eq 0 ]] || echo -n .
 c3out=camera3/$(date +%Y)/$(date +%m)/$(date +%d)
 mkdir -p $c3out
-wget ${=wget_opts} $zipurl/camera3.jpg > $c3out/camera3_${d}.jpg
+wget ${=wget_opts} $zipurl/hyrum_dam_se.jpg > $c3out/camera3_${d}.jpg
 
 [[ $VERBOSE -eq 0 ]] || echo done
