10355f9fa56816a2dbbcd39ff450a2b26c4ed892
Mitchel Humpherys <mitch.special@gmail.com>
2015-06-28 21:11:26 -0700

    make_daily.sh: initial 5-cam support

--- make_daily.sh
+++ make_daily.sh
@@ -7,6 +7,7 @@ output=${output:-$the_year-$the_month-$the_day.mp4}
 do_cams=${do_cams:-1}
 # do_3x1=${do_3x1:-1}
 do_1top_2bottom=${do_1top_2bottom:-1}
+do_4around_1center=${do_4around_1center:-1}
 verbose=${verbose:-0}
 do_upload=${do_upload:-y}
 venv=${venv:-~/virtualenvs/zipperdog/bin/activate}
@@ -27,6 +28,8 @@ if [[ $do_cams -eq 1 ]]; then
             camera1) readable="Camera 1" ;;
             camera2) readable="Camera 2" ;;
             camera3) readable="Camera 3" ;;
+            camera4) readable="Camera 4" ;;
+            camera5) readable="Camera 5" ;;
         esac
         [[ $do_upload = y ]] && ./upload_video.py \
             --file $cam/dailies/damcamdaily-$cam-$output \
@@ -51,12 +54,18 @@ for img in $(ls -1tr camera1/$the_year/$the_month/$the_day/*); do
     vlog -n "\b$iter"
     cam2_img=camera2/$the_year/$the_month/$the_day/camera2_${${img##*/}##*_}
     cam3_img=camera3/$the_year/$the_month/$the_day/camera3_${${img##*/}##*_}
+    cam4_img=camera3/$the_year/$the_month/$the_day/camera4_${${img##*/}##*_}
+    cam5_img=camera3/$the_year/$the_month/$the_day/camera5_${${img##*/}##*_}
     if [[ $(du $img | awk '{print $1}') -gt 0 && \
         $({ du $cam2_img 2>/dev/null || echo 0; } | awk '{print $1}') -gt 0 && \
-        $({ du $cam3_img 2>/dev/null || echo 0; } | awk '{print $1}') -gt 0 ]]; then
+        $({ du $cam3_img 2>/dev/null || echo 0; } | awk '{print $1}') -gt 0 && \
+        $({ du $cam4_img 2>/dev/null || echo 0; } | awk '{print $1}') -gt 0 && \
+        $({ du $cam5_img 2>/dev/null || echo 0; } | awk '{print $1}') -gt 0 ]]; then
         cam1_imgs+=($img)
         cam2_imgs+=($cam2_img)
         cam3_imgs+=($cam3_img)
+        cam4_imgs+=($cam4_img)
+        cam5_imgs+=($cam5_img)
     fi
 done
 vlog -e " Done\n"
@@ -69,6 +78,8 @@ ffmpeg_from_imgs()
         -f image2pipe -vcodec mjpeg -r 15 -i <(cat ${cam1_imgs[@]}) \
         -f image2pipe -vcodec mjpeg -r 15 -i <(cat ${cam2_imgs[@]}) \
         -f image2pipe -vcodec mjpeg -r 15 -i <(cat ${cam3_imgs[@]}) \
+        -f image2pipe -vcodec mjpeg -r 15 -i <(cat ${cam4_imgs[@]}) \
+        -f image2pipe -vcodec mjpeg -r 15 -i <(cat ${cam5_imgs[@]}) \
         -loop 1 -i banner.jpg \
         -filter_complex $filter \
         -vcodec libx264 \
@@ -84,14 +95,26 @@ ffmpeg_from_imgs()
 rollin="(h/2)-(500*((1/5)^t))"
 # font="Sail-Regular.ttf"
 font="Satisfy-Regular.ttf"
-filter_1top_2bottom="[3] scale=h=1080:w=-1, pad=w=1920:h=1080, trim=end=4, drawtext=fontfile=${font}: fontsize=150: x=(w/2)-(tw/2): y=${rollin}: text=HyrumDamCam.com, setsar=sar=1/1, fade=type=out:start_time=3:duration=1 [title]; [1]scale=w=-1:h=440, pad=w=1920:h=1080:x=178:y=640 [left_bottom]; [2] scale=w=-1:h=440 [right_bottom]; [left_bottom][right_bottom] overlay=x=960:y=640 [bottom]; [0] scale=w=-1:h=640 [top_middle]; [bottom][top_middle] overlay=x=391, setsar=sar=1/1, fade=duration=0.25 [main]; [title][main] concat=n=2"
+filter_1top_2bottom="[5] scale=h=1080:w=-1, pad=w=1920:h=1080, trim=end=4, drawtext=fontfile=${font}: fontsize=150: x=(w/2)-(tw/2): y=${rollin}: text=HyrumDamCam.com, setsar=sar=1/1, fade=type=out:start_time=3:duration=1 [title]; [1]scale=w=-1:h=440, pad=w=1920:h=1080:x=178:y=640 [left_bottom]; [2] scale=w=-1:h=440 [right_bottom]; [left_bottom][right_bottom] overlay=x=960:y=640 [bottom]; [0] scale=w=-1:h=640 [top_middle]; [bottom][top_middle] overlay=x=391, setsar=sar=1/1, fade=duration=0.25 [main]; [title][main] concat=n=2"
+filter_4around_1center="\
+[5] scale=h=1080:w=-1, pad=w=1920:h=1080, trim=end=4, drawtext=fontfile=${font}: fontsize=150: x=(w/2)-(tw/2): y=${rollin}: text=HyrumDamCam.com, setsar=sar=1/1, fade=type=out:start_time=3:duration=1 [title];
+[0] scale=w=iw/2:h=-1, pad=w=1920:h=1080:x=0:y=0 [top_left];
+[1] scale=w=iw/2:h=-1, pad=w=1920:h=1080:x=iw/2:y=0 [top_right];
+[2] scale=w=iw/2:h=-1, pad=w=1920:h=1080:x=0:y=iw/2 [bottom_left];
+[3] scale=w=iw/2:h=-1, pad=w=1920:h=1080:x=iw/2:y=iw/2 [bottom_right]; // <<< Start here!
+[4] scale=w=-1:h=440 [middle];
+[left_bottom][right_bottom] overlay=x=960:y=640 [bottom];
+[0] scale=w=-1:h=1080 [middle];
+[bottom][top_middle] overlay=x=391, setsar=sar=1/1, fade=duration=0.25 [main];
+[title][main] concat=n=2"
 
 
 # [[ $do_3x1 -eq 1 ]] && ffmpeg_from_imgs $filter_3x1 combined_dailies/damcamdaily-combined-3x1-$output
 [[ $do_1top_2bottom -eq 1 ]] && ffmpeg_from_imgs $filter_1top_2bottom combined_dailies/damcamdaily-combined-1top_2bottom-$output
+[[ $do_4around_1center -eq 1 ]] && ffmpeg_from_imgs $filter_4around_1center combined_dailies/damcamdaily-combined-4around_1center-$output
 
 [[ $do_upload = y ]] && ./upload_video.py \
-    --file combined_dailies/damcamdaily-combined-1top_2bottom-$output \
+    --file combined_dailies/damcamdaily-combined-4around_1center-$output \
     --title "Hyrum Dam Cam $the_year-$the_month-$the_day All Cameras" \
     --description "Visit http://www.hyrumdamcam.com for more info" \
     --noauth_local_webserver
